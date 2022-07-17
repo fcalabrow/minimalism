04ace52ccc4cca1cc64ed57712e78eb2635a58a9
Facundo Calabró <facundocalabro@gmail.com>
2022-07-17 16:50:50 -0300

    Modifico función principal

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 from derivations import *
 from structures import *
 
+# def main():
+#     lexicon = Lexicon()
+#     counter = 0
+#     ug = UniversalGrammar(set(), set(), set()) # todo: add feature import to UG
+#     i_lang = ILanguage(lexicon, ug)
+#     derivation = Derivation(i_lang, word_list=list(i_lang.lexicon.lex))
+#     derivation.derive()
+
 def main():
-    lexicon = Lexicon()
-    counter = 0
-    ug = UniversalGrammar(set(), set(), set()) # todo: add feature import to UG
-    i_lang = ILanguage(lexicon, ug)
-    derivation = Derivation(i_lang, word_list=list(i_lang.lexicon.lex))
-    derivation.derive()
-
-
-    # kate_token1 = LexicalItemToken(list(lexicon.lex)[0], 0)
-    # kate_token2 = LexicalItemToken(list(lexicon.lex)[0], 1)
-    # kate_runs = kate_token1.merge(kate_token2, 2)
-    # tr = tree(kate_runs)
-    # tr.pretty_print()
-
+    while True:
+        sentence = input("Insert a sentence to parse or press (f) to finish: ")
+        if sentence != 'f':
+            try:
+                parse(sentence)
+            except AssertionError:
+                print("Some words are not in the lexicon")
+        else:
+            break
 
 main()
