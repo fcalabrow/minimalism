10157f9f0726ae3f27d177a5810dde72f074da23
Facundo Calabró <facundocalabro@gmail.com>
2022-07-31 10:55:28 -0300

    Agrego stage-by-stage view

--- main.py
+++ main.py
@@ -32,13 +32,15 @@ def manual_derivation(filename=filename):
 
 def main():
     global filename
+    debug = False
+    enable = 'Enable'
     while True:
         new_line = "\n"
-        user_input = input(f'Select an option and press enter:{new_line*2}1 - Parser{new_line}2 - Manual derivation{new_line}3 - Change default grammar{new_line}4 - Quit{new_line*2}')
+        user_input = input(f'Select an option and press enter:{new_line*2}1 - Parser{new_line}2 - Manual derivation{new_line}3 - Change default grammar{new_line}4 - {enable} stage-by-stage view{new_line}5 - Quit{new_line*2}')
         if user_input == '1':
-            sentence = input(f'Insert a sentence to parse:{new_line}')
+            sentence = input(f'{new_line}Insert a sentence to parse:{new_line*2}')
             try:
-                parse(sentence,filename)
+                parse(sentence,filename,debug)
             except AssertionError:
                 print(f"{new_line}[ERROR] Some words are not in the lexicon{new_line}")
         elif user_input == '2':
@@ -53,6 +55,12 @@ def main():
                 except:
                     print(f'{new_line}[ERROR] Insert a valid option{new_line}')
         elif user_input == '4':
+            debug = not debug
+            if debug == False:
+                enable = "Enable"
+            else:
+                enable = 'Disable'
+        elif user_input == '5':
             break
         else:
             print('Select a valid option (1-4)')
